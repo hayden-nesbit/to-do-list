3ebd69d8d5f753018d617eb8d22a191102c65482
Hayden Nesbit <hayden.nesbit@campusoutreach.org>
2020-03-20 13:43:25 -0400

    starting work on to-do

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+import Input from './input'
+import TodoApp from './To-Do';
+
+class App extends React.Component {
+ 
+  render () {
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <TodoApp />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+    )};
 }
 
 export default App;
